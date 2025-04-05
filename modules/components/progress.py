3c170e3a7e93c995f99c3b530a9f993cef479378
Felix Kappeler <felix.kappeler@student.unisg.ch>
2025-04-05 12:04:29 +0800

    fix: correct bahaviour of study progress bar

--- modules/components/progress.py
+++ modules/components/progress.py
@@ -1,8 +1,14 @@
 import streamlit as st
 
 def show_progress():
-    progress = min(st.session_state.trial / st.session_state.max_trials, 1.0)
+    if st.session_state.page == 'debrief':
+        progress = 1.0
+    else:
+        progress = min((st.session_state.trial - 1) / st.session_state.max_trials, 1.0)
+
     with st.container():
         st.markdown("<br><br>", unsafe_allow_html=True)
+        # st.caption(f"Study progress: {int(progress * 100)}% complete")
+        st.caption(f"Study progress:")
         st.progress(progress)
-        st.caption(f"Study progress: {int(progress * 100)}% complete")
+        
