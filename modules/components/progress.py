8a10629ca533ef43d165e422a97b4b793308beeb
Felix Kappeler <felix.kappeler@student.unisg.ch>
2025-04-07 15:37:19 +0800

    feat: adjusted the exmeriment trial steps UI components

--- modules/components/progress.py
+++ modules/components/progress.py
@@ -7,7 +7,7 @@ def show_progress():
         progress = min((st.session_state.trial - 1) / st.session_state.max_trials, 1.0)
 
     with st.container():
-        st.markdown("<br><br>", unsafe_allow_html=True)
+        # st.markdown("<br><br>", unsafe_allow_html=True)
         # st.caption(f"Study progress: {int(progress * 100)}% complete")
         st.caption(f"Study progress:")
         st.progress(progress)
